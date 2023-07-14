218eaeceba1c3628725738872c9f0f05321c0746
LARA <LARA@LAPTOP-KEIHFN01>
2023-07-14 12:31:15 -0300

    subida de prueba

--- resources/views/admin/loan.blade.php
+++ resources/views/admin/loan.blade.php
@@ -31,7 +31,7 @@
                         </div>									
                         @endif				
 						<div class="container" style="margin:0px; padding:10px; width:100%">
-							<form  id="loan" action="{{route('admin_loan')}}" method="get" style="display:flex; flex-direction:row;justify-content: space-between;">
+							<form  id="contactForm" action="{{route('admin_loan')}}" method="get" style="display:flex; flex-direction:row;justify-content: space-between;">
 									<div style="width:30%" >
 										<input type="text" placeholder="Nombre del profesional" maxlength="100" class="form-control" name="name" id="name">
 									</div>
@@ -61,10 +61,45 @@
 										<a href="{{ route('loan_new_admin') }}">
 											<button id="addToTable" class="btn btn-primary">Nuevo Prestamo </button>
 										</a>
+										<a href="{{ route('loan_debt_admin') }}">
+											<button id="addToTable" class="btn btn-secondary">Actualizar Deudas</button>
+										</a>
 									</div>
 								</div>
 						</div>
-					
+						
+						<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 						<br><br>
 						@if($loans)
 						<div  style="padding:10px;flex-wrap: wrap;display:flex;flex-direction:row; justify-content:space-arround; margin: rigth 20px;">	
@@ -98,14 +133,14 @@
 													<!-- <li class="list-group-item">PROFESIONAL</li> -->
 													<li class="list-group-item">{{$loan->name}} {{$loan->lastName}}</li>
 													@if($loan->state_id == 1)
-													<a style="text-align:center; width:100%; margin-bottom: 30px;" href="{{ route('admin_loan_enable', ['loan_id' => $loan->loanId,'state' => 2] ) }}" class="btn btn-success"><i class="bi bi-hand-thumbs-up-fill">Rechazar</i></a>					
+													<a style="text-align:center; width:100%; margin-bottom: 30px;" href="{{ route('admin_loan_enable', ['loan_id' => $loan->loanId,'state' => 2] ) }}" class="btn btn-danger"><i class="bi bi-hand-thumbs-up-fill">Rechazar</i></a>					
 													@elseif($loan->state_id == 3)
 													<div style="display:flex; flex-direction:row">
 													<a style="text-align:center; width:50%; margin-bottom: 30px;" href="{{ route('admin_loan_enable', ['loan_id' => $loan->loanId,'state' => 1] ) }}" class="btn btn-success"><i class="bi bi-hand-thumbs-up-fill">Habilitar</i></a>					
 													<a style="text-align:center;width:50%; margin-bottom: 30px;" href="{{ route('admin_loan_enable',['loan_id' => $loan->loanId, 'state' => 2] ) }}" class="btn btn-danger"> Rechazar </a>													
 													</div>
-													@else
-													<a style="text-align:center;width:100%; margin-bottom: 30px;" href="{{ route('admin_loan_enable',['loan_id' => $loan->loanId, 'state' => 2] ) }}" class="btn btn-danger"> Rechazar</a>
+													@elseif($loan->state_id == 2)
+													<a style="text-align:center;width:100%; margin-bottom: 30px;" href="{{ route('admin_loan_enable',['loan_id' => $loan->loanId, 'state' => 1] ) }}" class="btn btn-success"> Habilitar</a>
 													@endif
 													
 										</ul>
@@ -131,3 +166,15 @@
 @endsection
 
 
+<script>
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+</script>
