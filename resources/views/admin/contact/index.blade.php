976d318ddccf76d86bd7f0dd86d4e744c4636f11
drodriguez <drodriguez@bienesdelestado.gob.ar>
2023-12-26 17:02:48 -0300

    se agrega el create en sitio provisoriamente 2

--- resources/views/admin/contact/index.blade.php
+++ resources/views/admin/contact/index.blade.php
@@ -40,11 +40,11 @@
                                 {{ __('Contact') }}
                             </span>
                              <div class="float-right">
-                                <!--
+                                
                                 <a href="{{ route('contacts.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
                                   {{ __('Create New') }}
                                 </a>
-                                -->
+                                
                             </div>
                         </div>
                     </div>
