08a8efca85dc224f37685445fdbb0f6b096524df
drodriguez <drodriguez@bienesdelestado.gob.ar>
2024-01-03 19:54:00 -0300

    se utiliza esta linea en el controlador de categoria

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -13,7 +13,8 @@ use Illuminate\Validation\Rules\Unique;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\File;
 use Mail;
-use Intervention\Image\Facades\Image as Image;;
+use Intervention\Image\Facades\Image as Image;
+//use Intervention\Image\Image;
 
 class CategoriaController extends Controller
 {
